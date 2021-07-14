e2027fed178a6e6ee60fe2e736cd4335595d7de5
Julian Horner <julianhorner@web.de>
2021-07-14 15:31:32 +0200

    Adapt to recent changes

--- actions/actions.py
+++ actions/actions.py
@@ -4,20 +4,25 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
-from weather import Weather
+from weather import getWeatherData
 
-class WeatherAction(Action):
-
+class TemperatureAction(Action):
+#------------------------------------------------------------------------------
     def name(self) -> Text:
-        return "weather_action"
+        return "temperature_action"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, 
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         city = tracker.get_slot('location')
-        temperature=Weather(city)['temp']
-        response = "Die aktuelle Temperatur in {} ist {} Grad Celsius.".format(city,temperature)
+
+        mainData = getWeatherData(city)['main']
+        temperature = mainData['temp']
+        response = "Die aktuelle Temperatur in {} ist {} Grad Celsius.".format(
+            city,temperature)
         
         dispatcher.utter_message(response)
 
         return [SlotSet('location',city)]
+
+#------------------------------------------------------------------------------
