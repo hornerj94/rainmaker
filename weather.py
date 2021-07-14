d141f3493ccc57ea3cca4760081fe54fef82da91
Julian Horner <julianhorner@web.de>
2021-07-14 15:56:47 +0200

    Improve readability of weather.py

--- weather.py
+++ weather.py
@@ -4,10 +4,12 @@ def getWeatherData(city):
 #------------------------------------------------------------------------------
 
     api_key = "bc6351bf115e5384ff7a8e9c1525b758"
-    base_url = "http://api.openweathermap.org/data/2.5/weather?lang=de"
-    final_url = base_url + "appid=" + api_key + "&q=" + city + "&units=metric"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    final_url = base_url + "appid=" + api_key + "&q=" + city + "&units=metric" \
+         + "&lang=de"
 
     weather_data = requests.get(final_url).json()
         
     return weather_data
+    
 #------------------------------------------------------------------------------
