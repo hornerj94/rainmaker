e039fc1047d69ba532fafea7cbbd1e82340cae26
Julian Horner <julianhorner@web.de>
2021-07-14 15:31:04 +0200

    Rename weather function to getWeatherData

--- weather.py
+++ weather.py
@@ -1,10 +1,13 @@
 import requests
 
-def Weather(city):
-    API_key = "bc6351bf115e5384ff7a8e9c1525b758"
-    base_url = "http://api.openweathermap.org/data/2.5/weather?"
-    
-    Final_url = base_url + "appid=" + API_key + "&q=" + city + "&units=metric"
-    weather_data = requests.get(Final_url).json()
-    
-    return weather_data['main']
+def getWeatherData(city):
+#------------------------------------------------------------------------------
+
+    api_key = "bc6351bf115e5384ff7a8e9c1525b758"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?lang=de"
+    final_url = base_url + "appid=" + api_key + "&q=" + city + "&units=metric"
+
+    weather_data = requests.get(final_url).json()
+        
+    return weather_data
+#------------------------------------------------------------------------------
