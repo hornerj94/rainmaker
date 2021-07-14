4ac4eaacfc4545c3f978057ada286a4856f12ec9
Julian Horner <julianhorner@web.de>
2021-07-14 15:57:17 +0200

    Correct current temperatur action name

--- actions/actions.py
+++ actions/actions.py
@@ -6,17 +6,18 @@ from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 from weather import getWeatherData
 
-class TemperatureAction(Action):
+class CurrentTemperatureAction(Action):
 #------------------------------------------------------------------------------
     def name(self) -> Text:
-        return "temperature_action"
+        return "current_temperature_action"
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, 
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         city = tracker.get_slot('location')
 
-        mainData = getWeatherData(city)['main']
+        data = getWeatherData(city)
+        mainData = data['main']
         temperature = mainData['temp']
         response = "Die aktuelle Temperatur in {} ist {} Grad Celsius.".format(
             city,temperature)
